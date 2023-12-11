0536ace95329f6854558d1e5ed99c0f2760ed816
Pratik <pratiksolanki2021@gmail.com>
2023-12-11 11:26:52 +0530

    initial setup

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import { Button } from "flowbite-react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen w-screen">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
